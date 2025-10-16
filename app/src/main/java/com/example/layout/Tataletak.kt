3c8922743e7774dc9a1df6e55b8c649741f24b08
Viaasyafrina16 <syafrinavia@gmail.com>
2025-10-16 12:39:14 +0700

    First Commit

---
+++ app/src/main/java/com/example/layout/Tataletak.kt
@@ -0,0 +1,28 @@
+package com.example.layout
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun TataletakColumn(modifier: Modifier) {
+    Column(modifier = modifier.padding(top = 50.dp)) {
+        Text(text =  "komponen1")
+        Text(text =  "komponen2")
+        Text(text =  "komponen3")
+        Text(text =  "komponen4")
+    }
+}
+
+@Composable
+fun TataletakColumn2(modifier: Modifier) {
+    Column(modifier = modifier.padding(top = 50.dp)) {
+        Text(text =  "komponen1")
+        Text(text =  "komponen2")
+        Text(text =  "komponen3")
+        Text(text =  "komponen4")
+    }
+}
